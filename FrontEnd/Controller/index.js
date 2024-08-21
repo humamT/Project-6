4b78c7b7212bf51d018b06ec8fafeaa47f82e7f1
humamT <humam.01@hotmail.com>
2024-08-21 10:50:43 +0200

    categories created and the next step is to add images from localstorage

--- FrontEnd/Controller/index.js
+++ FrontEnd/Controller/index.js
@@ -5,3 +5,55 @@ import {
 import {
     fetchWorks
 } from "../Model/works.js"
+
+//// FILTERS //// 
+
+const createCategoryElement = function (category) {
+    const button = document.createElement("button");
+    button.innerText = category.name
+    button.setAttribute("data-id", category.id)
+    return button
+}
+
+const initFilters = async function () {
+    let categories = await fetchCategories()
+    let categoriesElements = categories.map(createCategoryElement)
+    const filter = document.querySelector(".filter")
+    filter.appendChild(createCategoryElement({
+        id: 0,
+        name: "Tous"
+    }))
+    categoriesElements.forEach((category) => {
+        filter.appendChild(category)
+    })
+
+    // Selecting the filter buttons and the filterable images
+
+    const filterButtons = document.querySelectorAll(".filter button");
+    filterButtons[0].classList.add("active")
+    const gallery = document.querySelectorAll(".gallery figure");
+
+    // Define the filterimages function
+    const filterimages = (e) => {
+        let active = document.querySelector(".active")
+        if (active) active.classList.remove("active");
+        e.target.classList.add("active");
+        // Optional: to test the selected elements console.log(e.target);
+
+        // iteratte over each filterable image
+        gallery.forEach(figure => {
+            // add "hide" class to the card initially
+            figure.classList.add("hide");
+            // check if the figure matches the selected filter or "all" is selected
+            if (figure.dataset.id === e.target.dataset.id || e.target.dataset.id === "0") {
+                figure.classList.remove("hide");
+            }
+        })
+    };
+
+    // Add click event listener to each filter button
+    filterButtons.forEach(button => button.addEventListener("click", filterimages));
+
+    // Optional: to test the selected elements console.log(filterButtons, gallery);
+}
+initFilters();
--- FrontEnd/Model/catagories.js
+++ FrontEnd/Model/catagories.js
@@ -1,29 +1,15 @@
 export async function fetchCategories() {
-    // Implementation of fetchCategories if needed
-}
 
-// Selecting the filter buttons and the filterable images
-const filterButtons = document.querySelectorAll(".filter button");
-const gallery = document.querySelectorAll(".gallery figure");
-
-// Define the filterimages function
-const filterimages = (e) => {
-    document.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
-    // Optional: to test the selected elements console.log(e.target);
-
-    // iteratte over each filterable image
-    gallery.forEach(figure => {
-        // add "hide" class to the card initially
-        figure.classList.add("hide");
-        // check if the figure matches the selected filter or "all" is selected
-        if(figure.dataset.name === e.target.dataset.name || e.target.dataset.name === "Tous"){
-            figure.classList.remove("hide");
+    const response = await fetch("http://localhost:5678/api/categories", {
+        method: "get",
+        headers: {
+            'Content-Type': 'application/json'
         }
     })
-};
-
-// Add click event listener to each filter button
-filterButtons.forEach(button => button.addEventListener("click", filterimages));
-
-// Optional: to test the selected elements console.log(filterButtons, gallery);
+    if (!response.ok) {
+        return false;
+    }
+    const data = await response.json();
+    return data
+}
+// Implementation of fetchCategories if needed
